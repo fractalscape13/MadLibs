0f17bdebcb1b1fca1cefc1cd67712748e7a334a3
Joseph Wangemann <fractalscape13@gmail.com>
2020-03-09 12:27:10 -0700

    create initial file structure

---
+++ MadLibs/Program.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MadLibs
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
+    }
+  }
+}
